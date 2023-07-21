40b5517100342d5a8ba81d32ab35377df80b1476
Yurii Skrynnykov <truef1s7@gmail.com>
2023-07-22 00:09:38 +0300

    Revert dominoes problem

--- elixir/dominoes/lib/dominoes.ex
+++ elixir/dominoes/lib/dominoes.ex
@@ -6,21 +6,6 @@ defmodule Dominoes do
   possible to make a full chain
   """
   @spec chain?(dominoes :: [domino]) :: boolean
-  def chain?([]), do: true
-  def chain?([{a, b}]), do: a == b
-
   def chain?(dominoes) do
-    [first | dominoes] = dominoes
-    # [first | dominoes] = Enum.sort(dominoes)
-
-    IO.inspect(%{first: first, dominoes: dominoes}, label: "dominoes")
-    Enum.reduce(dominoes, [first], fn {a, b}, [{a_befor, b_befor} | rest] = acc ->
-      cond do
-        a_befor == a -> [{b, a} | acc]
-        a_befor == b -> [{a, b} | acc]
-        b_befor == b -> [{a, b}, {b_befor, a_befor} | rest]
-        true -> acc
-      end
-    end)
   end
 end
