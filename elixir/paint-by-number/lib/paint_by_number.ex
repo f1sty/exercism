c1071b63f66eb8538b2967400d0a6b0c9f4dd4ff
Yurii Skrynnykov <truef1s7@gmail.com>
2023-07-22 00:04:04 +0300

    Add paint_by_number solution

--- elixir/paint-by-number/lib/paint_by_number.ex
+++ elixir/paint-by-number/lib/paint_by_number.ex
@@ -1,29 +1,43 @@
 defmodule PaintByNumber do
   def palette_bit_size(color_count) do
-    # Please implement the palette_bit_size/1 function
+    color_count
+    |> :math.log2()
+    |> ceil()
   end
 
   def empty_picture() do
-    # Please implement the empty_picture/0 function
+    <<>>
   end
 
   def test_picture() do
-    # Please implement the test_picture/0 function
+    <<0::2, 1::2, 2::2, 3::2>>
   end
 
   def prepend_pixel(picture, color_count, pixel_color_index) do
-    # Please implement the prepend_pixel/3 function
+    palette = palette_bit_size(color_count)
+
+    <<pixel_color_index::size(palette), picture::bitstring>>
   end
+
+  def get_first_pixel(<<>>, _color_count), do: nil
 
   def get_first_pixel(picture, color_count) do
-    # Please implement the get_first_pixel/2 function
+    palette = palette_bit_size(color_count)
+
+    <<pixel::size(palette), _rest::bitstring>> = picture
+    pixel
   end
 
+  def drop_first_pixel(<<>>, _color_count), do: empty_picture()
+
   def drop_first_pixel(picture, color_count) do
-    # Please implement the drop_first_pixel/2 function
+    palette = palette_bit_size(color_count)
+
+    <<_pixel::size(palette), picture::bitstring>> = picture
+    picture
   end
 
   def concat_pictures(picture1, picture2) do
-    # Please implement the concat_pictures/2 function
+    <<picture1::bitstring, picture2::bitstring>>
   end
 end
