cbe669974784582c898bff648b6a7e7e3bb84fe9
Yurii Skrynnykov <truef1s7@gmail.com>
2023-07-22 12:18:36 +0300

    Refactor dominoes

--- elixir/dominoes/lib/dominoes.ex
+++ elixir/dominoes/lib/dominoes.ex
@@ -8,50 +8,18 @@ defmodule Dominoes do
   @spec chain?(dominoes :: [domino]) :: boolean
   def chain?([]), do: true
 
-  def chain?(dominoes) do
-    dominoes
-    |> Enum.map(&chain?(&1, List.delete(dominoes, &1)))
-    |> Enum.any?()
+  def chain?([domino | dominoes]) do
+    chain?(domino, dominoes)
   end
 
   defp chain?({edge, edge}, []), do: true
 
-  defp chain?(edges, pile) do
-    with update_instructions <- maybe_find_matching_domino(pile, edges),
-         {edges, pile} <- maybe_update_edges(update_instructions) do
-      chain?(edges, pile)
-    end
-  end
-
-  defp maybe_find_matching_domino(pile, {first, last} = edges) do
-    pile
-    |> Enum.with_index()
-    |> Enum.reduce(:none, fn {{edge1, edge2} = domino, idx}, update_instructions ->
-      cond do
-        first == edge2 -> {:prepend, domino, edges, pile, idx}
-        last == edge1 -> {:append, domino, edges, pile, idx}
-        first == edge1 -> {:prepend, rotate(domino), edges, pile, idx}
-        last == edge2 -> {:append, rotate(domino), edges, pile, idx}
-        true -> update_instructions
-      end
+  defp chain?({first, last}, pile) do
+    Stream.map(pile, fn
+      {edge1, ^last} = domino -> chain?({first, edge1}, List.delete(pile, domino))
+      {^last, edge2} = domino -> chain?({first, edge2}, List.delete(pile, domino))
+      _ -> false
     end)
+    |> Enum.any?()
   end
-
-  defp maybe_update_edges(:none), do: false
-
-  defp maybe_update_edges({:prepend, {first, _}, {_, last}, pile, idx}) do
-    edges = {first, last}
-    pile = List.delete_at(pile, idx)
-
-    {edges, pile}
-  end
-
-  defp maybe_update_edges({:append, {_, last}, {first, _}, pile, idx}) do
-    edges = {first, last}
-    pile = List.delete_at(pile, idx)
-
-    {edges, pile}
-  end
-
-  defp rotate({first, last}), do: {last, first}
 end
