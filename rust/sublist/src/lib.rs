38a166c5c374cc70715242bf1573faa4cc9adf5d
Yurii Skrynnykov <truef1s7@gmail.com>
2023-07-23 15:54:36 +0300

    Add sublist solution

--- rust/sublist/src/lib.rs
+++ rust/sublist/src/lib.rs
@@ -7,13 +7,27 @@ pub enum Comparison {
 }
 
 pub fn sublist<T: PartialEq>(first_list: &[T], second_list: &[T]) -> Comparison {
-    if first_list == second_list {
-        Comparison::Equal
-    } else if first_list.len() > second_list.len() {
-        let mut first_list = first_list.clone();
-        let mut second_list = second_list.clone();
-        let sublist_start_elem = second_list[0];
-        let sublist_start_idx = second_list.iter().find(|&&elem| elem == sublist_start_elem).unwrap();
-    }
+    let first_list_len = first_list.len();
+    let second_list_len = second_list.len();
 
+    if first_list_len > second_list_len && is_sublist(first_list, second_list, second_list_len) {
+        Comparison::Superlist
+    } else if first_list_len < second_list_len
+        && is_sublist(second_list, first_list, first_list_len)
+    {
+        Comparison::Sublist
+    } else if first_list == second_list {
+        Comparison::Equal
+    } else {
+        Comparison::Unequal
+    }
+}
+
+fn is_sublist<T: PartialEq>(bigger_list: &[T], smaller_list: &[T], window_size: usize) -> bool {
+    if bigger_list.is_empty() || smaller_list.is_empty() {
+        return true;
+    }
+    bigger_list
+        .windows(window_size)
+        .any(|window| window == smaller_list)
 }
