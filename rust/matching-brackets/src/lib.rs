64a53fb39118061df39c859f65ed7f57ec9d482b
Yurii Skrynnykov <truef1s7@gmail.com>
2023-07-26 21:19:38 +0300

    Update matching-brackets solution

--- rust/matching-brackets/src/lib.rs
+++ rust/matching-brackets/src/lib.rs
@@ -2,9 +2,7 @@ pub fn brackets_are_balanced(string: &str) -> bool {
     let mut unclosed: Vec<char> = vec![];
 
     string.chars().for_each(|chr| match chr {
-        '[' => unclosed.push('['),
-        '(' => unclosed.push('('),
-        '{' => unclosed.push('{'),
+        '[' | '(' | '{' => unclosed.push(chr),
         ']' => {
             if let Some(new_unclosed) = unclosed.strip_suffix(&['[']) {
                 unclosed = new_unclosed.to_vec();
