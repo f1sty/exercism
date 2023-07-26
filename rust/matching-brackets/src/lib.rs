556074c6d033fab36c4a34b17c800330568e8586
Yurii Skrynnykov <truef1s7@gmail.com>
2023-07-26 21:17:09 +0300

    Add matching-brackets solution

---
+++ rust/matching-brackets/src/lib.rs
@@ -0,0 +1,33 @@
+pub fn brackets_are_balanced(string: &str) -> bool {
+    let mut unclosed: Vec<char> = vec![];
+
+    string.chars().for_each(|chr| match chr {
+        '[' => unclosed.push('['),
+        '(' => unclosed.push('('),
+        '{' => unclosed.push('{'),
+        ']' => {
+            if let Some(new_unclosed) = unclosed.strip_suffix(&['[']) {
+                unclosed = new_unclosed.to_vec();
+            } else {
+                unclosed.push(']');
+            }
+        }
+        ')' => {
+            if let Some(new_unclosed) = unclosed.strip_suffix(&['(']) {
+                unclosed = new_unclosed.to_vec();
+            } else {
+                unclosed.push(')');
+            }
+        }
+        '}' => {
+            if let Some(new_unclosed) = unclosed.strip_suffix(&['{']) {
+                unclosed = new_unclosed.to_vec();
+            } else {
+                unclosed.push('}');
+            }
+        }
+        _ => (),
+    });
+
+    unclosed.is_empty()
+}
